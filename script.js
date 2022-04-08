e1bc2e9709bd4fc3c937ce568299ff7f8066354a
Preet Panchal <58786109+PreetPanchal2022@users.noreply.github.com>
2022-04-08 01:26:25 -0400

    PokeAPI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const $pokedex = document.getElementById('.pokedex');
+const $pokedex = document.getElementById('pokedex');
+const $overlay = document.getElementById('overlay');
 
 //make a fetch call to PokeAPI
 //to get list of pokemon
@@ -18,12 +19,11 @@ const $pokedex = document.getElementById('.pokedex');
 //     })
 
 //for the pokemon api
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
     .then(function(response) {
         //request was successful
-        return response.json();
-    }
-    )
+        return response.json()
+    })
     .then(function(json) {
         //get the data
         // console.log(json);
@@ -33,10 +33,38 @@ fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
         const html = []
 
         for (const pokemon of json.results) {
-            html.push(`<a href="#" class="list-group-item list-group-item-action">
+            html.push(`<a href="#"
+            data-url="${pokemon.url}"
+             class="list-group-item list-group-item-action">
             ${pokemon.name}
             </a>`)
 
         }
-        $pokedex.innerHTML = html.join('')
+
+        $pokedex.innerHTML = html.join('');
     })
+
+    //add event listener to pokedex by adding an event listener to the list
+    //fetch the pokemon data from PokeAPI
+    $pokedex.addEventListener('click', 
+    async function(e) {
+        e.preventDefault();
+
+        if(e.target.classList.contains('list-group-item')) {
+            //request data using async/await
+            const response = await fetch(e.target.dataset.url);
+            const pokemon = await response.json();
+            // console.log(pokemon);
+
+            $overlay.innerHTML = `
+            <img
+            src="${pokemon.sprites.other['official-artwork'].front_default}"
+            alt="${pokemon.name}">`;
+
+            $overlay.classList.add('show');
+    }
+})
+
+$overlay.addEventListener('click', function() {
+    $overlay.classList.remove('show');
+})
